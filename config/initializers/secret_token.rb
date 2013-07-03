d31fbe18cbf0bd7ba1d14022718bad9938ac37ff
Andrew <andy@ziptrek.com>
2013-07-03 15:11:48 -0700

    initi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd1dd09113779b6b38ff56b865034aca78bbb0e827ab6142bcedff765a7e65fc791063df23e34447e39c367200eaf09bb933346fb9f5b0ce315179538682c83c6'
