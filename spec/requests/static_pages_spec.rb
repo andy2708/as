b8cc77c8ca19b7456cf2928afa91eb0315757d30
Andrew <andy@ziptrek.com>
2013-07-07 14:07:10 -0700

    finsihs layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,39 +2,45 @@ require 'spec_helper'
 
 describe "StaticPages" do
 	describe "Home Page" do
-		it "should have the content 'Sample App'" do
-		# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-			visit '/static_pages/home'
-			page.should have_content('Sample App')
-		end
-		it "should have the title 'Home'" do
-		# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-			visit '/static_pages/home'
-			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
-		end
+		before { visit root_path }
+		it {should have_selector('h1','Sample App')} 
+		it {should have_selector('title',text:'Ruby on Rails Tutorial | Home')} 
+			
 	end
 	
 	describe "help page" do
 		it "shold ahve the content 'help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_content('Help')
 		end	
 		it "should have the title 'Help'" do
 		# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-			visit '/static_pages/help'
-			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+			visit help_path
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial | Help")
 		end
 	end
 
 	describe "about page" do
 		it "shold have the content 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_content('About Us')
 		end	
 		it "should have the title 'About'" do
 		# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-			visit '/static_pages/about'
-			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
+			visit about_path
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial | About")
+		end
+	end
+	
+	describe "contact page" do
+		it "shold have the content 'Contact'" do
+			visit contact_path
+			page.should have_selector('h1',text:'Contact')
+		end	
+		it "should have the title 'Contact'" do
+		# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+			visit contact_path
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial | Contact")
 		end
 	end
 end
